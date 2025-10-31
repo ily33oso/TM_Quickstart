fcbbf9b55a984be4888115d07839f52a4849243b
Unnece55ary5face <166060662+Unnece55ary5face@users.noreply.github.com>
2025-10-30 17:53:48 -0700

    bleh
    asdjkh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/brandonsTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/brandonsTeleOp.java
@@ -45,7 +45,7 @@ public class brandonsTeleOp extends LinearOpMode {
         // Adjust the orientation parameters to match your robot
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
-                RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
         // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
         imu.initialize(parameters);
 
@@ -54,18 +54,22 @@ public class brandonsTeleOp extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double lx = gamepad1.left_stick_x;
-            double ly = gamepad1.left_stick_y;
-            double rx = gamepad1.right_stick_x;
+           
+            double y = -gamepad1.left_stick_y *  0.6; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x  * 0.6;
+            double rx = -gamepad1.right_stick_x * 0.6;
 
-            double max = Math.max(Math.abs(lx) + Math.abs(ly) + Math.abs(rx), 1);
 
-            double drivePower = 0.8 - (0.6 * gamepad1.right_trigger);
 
-            if (gamepadRateLimit.hasExpired() && gamepad1.y) {
+
+            // This button choice was made so that it is hard to hit on accident,
+            // it can be freely changed based on preference.
+            // The equivalent button is start on Xbox-style controllers.
+            if (gamepad1.options) {
                 imu.resetYaw();
-                gamepadRateLimit.reset();
             }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
             double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
             double adjustedLx = -ly * Math.sin(heading) + lx * Math.cos(heading);
@@ -75,6 +79,26 @@ public class brandonsTeleOp extends LinearOpMode {
             bl.setPower(((adjustedLy + adjustedLx + rx) / max) * drivePower);
             fr.setPower(((adjustedLy + adjustedLx - rx) / max) * drivePower);
             fl.setPower(((adjustedLy - adjustedLx + rx) / max) * drivePower);
+            
+                // Rotate the movement direction counter to the bot's rotation
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
             //what the sigma - Joel
 
             // vacuum in and get balls
